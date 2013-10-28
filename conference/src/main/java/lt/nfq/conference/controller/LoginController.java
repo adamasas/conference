234c78822391272b278f613b005fdfe5f413ccfb
adomas <adamsus@gmail.com>
2013-10-28 08:11:18 +0200

    fixed pom problems

--- conference/src/main/java/lt/nfq/conference/controller/LoginController.java
+++ conference/src/main/java/lt/nfq/conference/controller/LoginController.java
@@ -3,6 +3,7 @@ package lt.nfq.conference.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -13,8 +14,12 @@ public class LoginController {
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login() {
-    	
-        return "redirect:/conference/list";
+    public String login(@RequestParam("submitButton") String submit) {
+       	if (submit.equals("Register")){
+    		return "redirect:/register";
+    	}else{
+            return "redirect:/conference/list";
+    		
+    	}
     }
 }
